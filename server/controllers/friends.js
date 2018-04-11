6f7af98145bb7d9502f68c2fa62d8d7dd8472eec
Derek DeCoste <dsquaredslim@gmail.com>
2018-04-10 20:51:52 -0700

    react search component to controller to sql query all working

--- server/controllers/friends.js
+++ server/controllers/friends.js
@@ -1,7 +1,7 @@
 const con = require('../db/db.js');
 
-var searchFriends = function(req, res) { // search term passed in
-  var term = req.url.slice(12);
+var searchFriends = function(req, res) {
+  var term = req.url.slice(12); // escape this search term
   con.query(`SELECT * FROM greenfield.users WHERE full_name LIKE '%${term}%' OR username LIKE '%${term}%'`, function(err, res) {
     if (err) {
       //return cb('Error in query postMessage');
