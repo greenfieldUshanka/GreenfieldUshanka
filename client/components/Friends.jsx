a1dd80fe44c65dda1a2cf5385d58b04aabbf08b1
Derek DeCoste <dsquaredslim@gmail.com>
2018-04-11 16:42:07 -0700

    added addFriend framework

--- client/components/Friends.jsx
+++ client/components/Friends.jsx
@@ -1,8 +1,9 @@
 // have search bar here and on profile, either search will send search result to server,
 // which will route to correct endpoint in the controller, which will ping the database,
 // which will then route to this page and display results
-// state here will be search term and an array of people objects that will be displayed
+// state here will be array of people objects that will be displayed
 // ultimately, will want to show friends first in search results and then everyone else
+
 import React from 'react';
 import SearchBar from './SearchBar.jsx';
 import FriendCard from './FriendCard.jsx';
@@ -12,7 +13,7 @@ class Friends extends React.Component {
     super();
 
     this.state = {
-      friends: [], // this is the array that the search results and friends will show in
+      friends: [],
     };
     this.handleChange = this.handleChange.bind(this);
   }
