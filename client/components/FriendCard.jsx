56bbf0a573185303a077c451660a78dca6b42de0
Derek DeCoste <dsquaredslim@gmail.com>
2018-04-11 16:42:07 -0700

    added addFriend framework

--- client/components/FriendCard.jsx
+++ client/components/FriendCard.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+const axios = require('axios');
 
 class Card extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      buttonMessage: 'Add Friend',
+    };
+
+    this.addFriend = this.addFriend.bind(this);
+  }
+
+  addFriend() {
+    // ping server with request to add friend, server -> router -> controller sees if they are your friend
+    // and adds them if they aren't and sends back a message with the result regardless, and changes the
+    // button text with this message
+    console.log('trying to add ', this.props.friend.full_name, ' as a friend')
+
+    axios.post('/addfriend', {
+      potentialFriendId: this.props.friend.id, // change this to data I know to test friend logic
+      myId: 31, // this will be the id of the logged in user...using 31 bc it is in both columns of table
+    })
+    .then(function (res) {
+      console.log(res);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
   }
 
   render() {
@@ -18,9 +43,9 @@ class Card extends React.Component {
             <a>Drinks {this.props.friend.vodka_consumption} handles of vodka a week</a>
           </div>
         </div>
-        <div className="ui bottom attached button">
+        <div className="ui bottom attached button" onClick={this.addFriend}>
           <i className="add icon"></i>
-          Add Friend
+          {this.state.buttonMessage}
         </div>
       </div>
     )
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const friends = require('../controllers/friends.js');
 
 router.get('/friends', friends.searchFriends);
+router.post('/addfriend', friends.addFriend);
 
 module.exports = router;
