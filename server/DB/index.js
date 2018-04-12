6580dee975b9468e8f50f16e2b596624b95a47b7
IsabellaBeltran <isabellabeltran79@gmail.com>
2018-04-12 15:29:55 -0700

    Created dotenv file for our DB and Port

--- server/DB/index.js
+++ server/DB/index.js
@@ -1,10 +1,14 @@
 const mySql = require('mysql');
+const host = process.env.DB_HOST;
+const user = process.env.DB_USER; 
+const password = process.env.DB_PASS; 
+const database = process.env.DB_DATABASE;
 
 const connection = mySql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'greenfield'
+  host: host,
+  username: user,
+  password: password,
+  database: database
 });
 
 module.exports = connection; 
