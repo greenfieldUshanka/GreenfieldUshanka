fa12a53848f0d6b828833541e0e57f40d6fc167f
IsabellaBeltran <isabellabeltran79@gmail.com>
2018-04-14 14:26:50 -0700

    Eslint

--- server/controllers/personalInfoController.js
+++ server/controllers/personalInfoController.js
@@ -2,20 +2,22 @@ const DB = require('../DB');
 
 const PersonalInfoController = {
   GetProfileInformation: (req, res) => {
-    DB.query(`SELECT * from users WHERE id=?`, [req.params.id], (err, data) => {
-     if (err) {console.log('Error from personalInfo', err)}
-     res.status(200).send({
-       username: data[0].username, 
-       profilePic: data[0].profile_picture, 
-       work: data[0].work, 
-       join: data[0].created_at,
-       vodka: data[0].vodka_consumption
-      })
-    })
-
+    DB.query('SELECT * from users WHERE id=?', [req.params.id], (err, data) => {
+      if (err) { console.log('Error from personalInfo', err); }
+      res.status(200).send({
+        username: data[0].username, 
+        profilePic: data[0].profile_picture, 
+        work: data[0].work, 
+        join: data[0].created_at,
+        vodka: data[0].vodka_consumption
+      });
+    });
+  },
+  SaveUpdatedProfile: (req, res) => {
+    
   }
 
 
-}
+};
 
 module.exports = PersonalInfoController; 
