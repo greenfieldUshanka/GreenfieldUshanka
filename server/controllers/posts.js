76d6897a8d3466354d8bcd47379a388eec72e222
David Leung <6911277+davidleu@users.noreply.github.com>
2018-04-13 18:22:32 -0700

    Finished committing all the files.

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,28 +1,35 @@
 const db = require('../db');
 
-
 var savePosts = function (req, res) {
-  console.log('req.body ', req.body);
   var postContent = req.body;
-  db.query("INSERT INTO posts (post_text) VALUES (?)", [postContent.post_text], function (err, data) {
+  db.query('INSERT INTO posts (post_text, id_author, id_wall) VALUES (?, ?, ?)',
+    [postContent.postText, postContent.idAuthor, postContent.idWall],
+    function (err, data) {
     if (err) {
       res.send(err);
     } else {
       res.send(data);
     }
   });
-}
+};
 
 var fetchPosts = function (req, res) {
-  console.log('in the post controller, running fetchPosts');
-  db.query("SELECT * FROM posts", function (err, data) {
+  db.query('SELECT id, post_text, created_at, id_author FROM posts ORDER BY created_at DESC LIMIT 5', function (err, data) {
     if (err) {
       res.send(err);
     } else {
-      res.send(data);
+      res.send(data.map(row => {
+        console.log(row);
+        return {
+          id: row.id,
+          postText: row.post_text,
+          createdAt: row.created_at,
+          idAuthor: row.id_author
+        };
+      }));
     }
   });
-}
+};
 
 exports.fetchPosts = fetchPosts;
 exports.savePosts = savePosts;
