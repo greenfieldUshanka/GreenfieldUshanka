4e69fc7c672e4b15c8b3f54bc51381149f083898
IsabellaBeltran <isabellabeltran79@gmail.com>
2018-04-17 08:03:22 -0400

    Refactor some...

--- client/components/homepage/index.js
+++ client/components/homepage/index.js
@@ -115,8 +115,7 @@ class HomePage extends React.Component {
   }
 
   render() {
-    if (this.props.id === this.props.wallId){
-
+    if (this.props.id === this.props.wallId) {
       return (
         <div>
           <div className="container-full-page" >
@@ -134,21 +133,18 @@ class HomePage extends React.Component {
                   <div className='profile-picture'>
                     <Image src='https://source.unsplash.com/300x300/?people' size='medium' rounded >
                     </Image>
-
-
                     <div className='friends-list'>
-                    <Card.Group>
-
                     {
                       this.state.friends.length ? (
-
-                      this.state.friends.map(friend =>
+                        this.state.friends.map(friend =>
+                      <Card.Group>
                       <Card className='friend-entry'>
                         <Card.Header> 
                         <Image onClick={() => this.props.setWallId(friend.id)} floated='right' size='small' src={friend.profile_picture} key={friend.id} />
                           {friend.full_name.toUpperCase()}
                         </Card.Header>
                       </Card>
+                    </Card.Group>
                       )) : (
                         <Card className='friend-entry'>
                         <Card.Header> 
@@ -156,16 +152,15 @@ class HomePage extends React.Component {
                       </Card>
                       )
                     }
-                    </Card.Group>
                   </div>
                   </div>
-
-
                 </Grid.Column>
               </Grid.Row>
               <Grid.Row>
-                <Grid.Column width={6}>
-                  <div className='user-profile-information'>
+                {
+                  this.props.id === this.props.wallId ? 
+                  (<Grid.Column width={6}>
+                    <div className='user-profile-information'>
                     <Form onSubmit={this.saveUserEditInformation}>
                       <div className='upi-personal-info'>
                         <Icon name='world' size={'large'} />
@@ -208,41 +203,10 @@ class HomePage extends React.Component {
                       </div>
                     </Form>
                   </div>
-                </Grid.Column>
-                <Grid.Column width={10}>
-                  <PostInput id={this.props.id} wallId={this.props.wallId} fetchPostFeed={this.props.fetchPostFeed}/>
-                  <PostList id={this.props.id} posts={this.props.posts} fetchPostFeed={this.props.fetchPostFeed}/>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <div className="container-full-page" >
-            <Grid>
-              <Grid.Row>
-                <Grid.Column width={16}>
-                  <div>
-                    <Image src='https://source.unsplash.com/1600x400/?nature' rounded /> 
-                    <div className='username-on-image'><h1>{this.state.username.toUpperCase()}</h1></div>
-                  </div>
-                </Grid.Column >
-              </Grid.Row>
-  
-              <Grid.Row>
-                <Grid.Column >
-                  <div className='profile-picture'>
-                    <Image src='https://source.unsplash.com/300x300/?people' size='medium' rounded >
-                    </Image>
-                  </div>
-                </Grid.Column>
-              </Grid.Row>
-              <Grid.Row>
-                <Grid.Column width={6}>
-                  <div className='friends-profile-information'>
+                  </Grid.Column>)
+                  :
+                  (<Grid.Column width={6}>
+                    <div className='friends-profile-information'>
                     <Card>
                       <Card.Content header= {`Ushanka member since ${moment(this.state.join).fromNow()}`}/>
                       <Card.Content description={`Current status: ${this.state.mood} `} >
@@ -252,18 +216,17 @@ class HomePage extends React.Component {
                       <Card.Content maxLength="2" description={this.state.extra} />
                     </Card>
                   </div>
-                </Grid.Column>
-  
-  
-                <Grid.Column width={10}>
-                  <PostInput id={this.props.id} wallId={this.props.wallId} fetchPostFeed={this.props.fetchPostFeed}/>
-                  <PostList id={this.props.id} posts={this.props.posts} fetchPostFeed={this.props.fetchPostFeed}/>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
+                  </Grid.Column>)
+                }
+              <Grid.Column width={10}>
+                <PostInput id={this.props.id} wallId={this.props.wallId} fetchPostFeed={this.props.fetchPostFeed}/>
+                <PostList id={this.props.id} posts={this.props.posts} fetchPostFeed={this.props.fetchPostFeed}/>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
           </div>
-        </div>
-      );
+        </div> 
+      )
     }
   }
 }
