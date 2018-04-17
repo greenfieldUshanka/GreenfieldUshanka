0fe58b5818cf0de480e9bc2902d573842237540e
IsabellaBeltran <isabellabeltran79@gmail.com>
2018-04-17 06:19:36 -0400

    Added Home icon to get back to profile

--- client/components/appheader/index.js
+++ client/components/appheader/index.js
@@ -1,43 +1,44 @@
 import React from 'react';
-import {Form, Grid, Button} from 'semantic-ui-react';
+import {Form, Grid, Button, Input, Icon, Header} from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import './index.css';
 import SearchBar from '../SearchBar.jsx';
 
 
 const AppHeader = (props) => {
-  
-  return (
-    <header className='home-page-header'>
-      <Grid>
-        <Grid.Row>
-          <Grid.Column width={4} > 
-            <Form>
-              <Form.Field inline>
-                <h1>
-                  <Link className='name' to={'/main'}>ushanka</Link>
-                </h1>
-              </Form.Field>
-            </Form>
-          </Grid.Column>
-          <Grid.Column width={1}>
-          </Grid.Column>
-          <Grid.Column width={6} className='search-friends-bar' >
-            <SearchBar onChange={props.onChange} currentPage={props.currentPage} id={props.id}/>
-          </Grid.Column>
-          <Grid.Column width={1}>
-          </Grid.Column>
-          <Grid.Column width={1}>
-          </Grid.Column>
-          <Grid.Column width={1}>
-          </Grid.Column>
-          <Grid.Column width={2} >
-            <Button className='logout-button' onClick={props.logout} >Log out</Button>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </header>
-  );
+console.log(props)
+return (
+<header className='home-page-header'>
+<Grid>
+<Grid.Row>
+<Grid.Column width={4} >
+<Form>
+<Form.Field inline>
+<Header as='h6' icon>
+<Icon onClick={() => props.setWallId(props.id)} color={'grey'} name='home' />
+</Header>
+<Link className='name' to={'/main'}>ushanka</Link>
+</Form.Field>
+</Form>
+</Grid.Column>
+<Grid.Column width={1}>
+</Grid.Column>
+<Grid.Column width={6} className='search-friends-bar' >
+<SearchBar onChange={props.onChange} currentPage={props.currentPage} id={props.id}/>
+</Grid.Column>
+<Grid.Column width={1}>
+</Grid.Column>
+<Grid.Column width={1}>
+</Grid.Column>
+<Grid.Column width={1}>
+</Grid.Column>
+<Grid.Column width={2} >
+<Button className='logout-button' onClick={props.logout} >Log out</Button>
+</Grid.Column>
+</Grid.Row>
+</Grid>
+</header>
+);
 };
 
 export default AppHeader;
